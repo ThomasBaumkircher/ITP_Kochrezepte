54f96e3341d4f0043ea2598bb420087e3972c004
Thomasderguetige <baumki200025@sr.htlweiz.at>
2025-02-03 08:49:34 +0100

    included redirect uri in openid config

--- src/backend/core/openid_config.py
+++ src/backend/core/openid_config.py
@@ -9,6 +9,7 @@ azure_scheme = SingleTenantAzureAuthorizationCodeBearer(
     app_client_id=settings.APP_CLIENT_ID,
     tenant_id=settings.TENANT_ID,
     scopes=settings.SCOPES,
+    redirect_uri=settings.BACKEND_CORS_ORIGINS[0] + "/callback",
 )
 
 
