6660dbb5ffddbec6034576c468920cbd5a76dc61
Thomasderguetige <baumki200025@sr.htlweiz.at>
2025-03-17 09:13:03 +0100

    fixed warnings

--- src/backend/app/database/config.py
+++ src/backend/app/database/config.py
@@ -1,7 +1,6 @@
 from databases import Database
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 from setting.config import get_settings
 
